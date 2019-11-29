d8dda7b02f366835a6edfdaff76fcefe6fe08743
aaron shivers <aaorn@moonmail.org>
2019-11-29 10:43:22 -0600

    Add sidenav componnet

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'custom-material-them'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('custom-material-them');
+    expect(app.title).toEqual('custom-material-theme');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('custom-material-them app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('custom-material-theme app is running!');
   });
 });
